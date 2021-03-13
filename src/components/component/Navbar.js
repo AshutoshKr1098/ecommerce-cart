d118caef0294fff847c63f0bcd03cc901fdc7707
Ashutosh <ashu.heretocode@gmail.com>
2021-03-13 19:00:22 +0530

    updated syncing of navbar cart value with total items

--- src/components/component/Navbar.js
+++ src/components/component/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "../../assets/styles/Navbar.css";
-const Navbar = () => {
+const Navbar = (props) => {
+  const calculateTotalItemInCart = () => {
+    let totalItems = 0;
+    props.cart.map((item) => (totalItems += item.quantity));
+    return totalItems;
+  };
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -16,7 +22,10 @@ const Navbar = () => {
         <li>
           <Link to="/cart" className="cart__link">
             <span>
-              Cart <span className="cartlogo__badge">0</span>
+              Cart{" "}
+              <span className="cartlogo__badge">
+                {calculateTotalItemInCart()}
+              </span>
             </span>
           </Link>
         </li>
@@ -30,4 +39,7 @@ const Navbar = () => {
     </nav>
   );
 };
-export default Navbar;
+const mapStateToProps = (state) => {
+  return { cart: state.cart };
+};
+export default connect(mapStateToProps)(Navbar);
